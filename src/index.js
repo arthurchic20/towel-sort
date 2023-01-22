09efa209000818defaed405aa1e815320883025d
arthurchic20 <arthurchic20@gmail.com>
2023-01-22 16:01:41 +0600

    init: start and end project towelSort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
-  return [];
+  module.exports = function towelSort (matrix) {
+    let res = []
+    
+    if (matrix === undefined) {
+        return res
+    } else {
+        matrix.forEach((element, index) => {
+            if ( index == 0) {
+                res = res.concat(element)
+            } else if (index > 0  && index % 2 == 0 ) {
+                res = res.concat(element)
+            } else if (index > 0  && index % 2 != 0) {
+                res = res.concat(element.reverse())
+            }
+        }) 
+        return res
+    }
 }
+
